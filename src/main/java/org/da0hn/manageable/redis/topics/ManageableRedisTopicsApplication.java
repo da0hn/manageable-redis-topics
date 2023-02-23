96a4f07e8b753ccc9351f3e353386b9c880d6bb9
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-02-23 19:29:08 -0400

    chore: initial commit

---
+++ src/main/java/org/da0hn/manageable/redis/topics/ManageableRedisTopicsApplication.java
@@ -0,0 +1,13 @@
+package org.da0hn.manageable.redis.topics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageableRedisTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageableRedisTopicsApplication.class, args);
+	}
+
+}
