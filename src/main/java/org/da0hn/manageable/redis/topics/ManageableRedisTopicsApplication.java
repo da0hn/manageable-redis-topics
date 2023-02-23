44e032d0a17b97552dc3e1d3185a33a1b2bcdee0
Da0hn <gabrielhondacba@gmail.com>
2023-02-23 19:42:25 -0400

    refactor: reformat code

--- src/main/java/org/da0hn/manageable/redis/topics/ManageableRedisTopicsApplication.java
+++ src/main/java/org/da0hn/manageable/redis/topics/ManageableRedisTopicsApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ManageableRedisTopicsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManageableRedisTopicsApplication.class, args);
-	}
+  public static void main(final String[] args) {
+    SpringApplication.run(
+      ManageableRedisTopicsApplication.class,
+      args
+    );
+  }
 
 }
