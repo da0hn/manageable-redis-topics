4b2f7ea20f829790478b2a7a716247b1865058c7
Da0hn <gabrielhondacba@gmail.com>
2023-02-23 23:37:17 -0400

    feat: add implementation of `AsyncLongTask`

--- src/main/java/org/da0hn/manageable/redis/topics/tasks/AsyncLongTask.java
+++ src/main/java/org/da0hn/manageable/redis/topics/tasks/AsyncLongTask.java
@@ -1,4 +1,39 @@
 package org.da0hn.manageable.redis.topics.tasks;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Component
+@AllArgsConstructor
 public class AsyncLongTask {
+
+  public void execute() {
+
+    log.info("Iniciando execução da tarefa...");
+    IntStream.rangeClosed(
+      0,
+      100
+    ).forEach(i -> {
+      try {
+        log.info("Executando iteração {}", i);
+        log.info("Indo dormir...");
+        TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(
+          0,
+          5
+        ));
+      }
+      catch (final InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new RuntimeException(e);
+      }
+    });
+    log.info("Tarefa terminada.");
+  }
+
 }
