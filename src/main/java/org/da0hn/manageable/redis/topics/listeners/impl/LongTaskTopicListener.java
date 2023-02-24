19f76200c740fa182a19436128cc5c955507da91
Da0hn <gabrielhondacba@gmail.com>
2023-02-23 23:43:26 -0400

    feat: registry parallel task in `TaskRegister`

--- src/main/java/org/da0hn/manageable/redis/topics/listeners/impl/LongTaskTopicListener.java
+++ src/main/java/org/da0hn/manageable/redis/topics/listeners/impl/LongTaskTopicListener.java
@@ -1,13 +1,23 @@
 package org.da0hn.manageable.redis.topics.listeners.impl;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.da0hn.manageable.redis.topics.config.TaskRegister;
 import org.da0hn.manageable.redis.topics.listeners.Listener;
 import org.da0hn.manageable.redis.topics.messages.SimpleMessage;
+import org.da0hn.manageable.redis.topics.tasks.AsyncLongTask;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.FutureTask;
 
 @Slf4j
 @Component
+@AllArgsConstructor
 public class LongTaskTopicListener implements Listener<SimpleMessage> {
+
+  private final AsyncLongTask asyncLongTask;
+
+  private final TaskRegister taskRegister;
 
   @Override
   public void listen(final SimpleMessage data) {
@@ -15,6 +25,15 @@ public class LongTaskTopicListener implements Listener<SimpleMessage> {
       "Mensagem recebida {}. Iniciando task...",
       data
     );
+    final FutureTask<Void> task = new FutureTask<>(() -> {
+      this.asyncLongTask.execute();
+      return null;
+    });
+    this.taskRegister.put(
+      data.getId().toString(),
+      task
+    );
+    task.run();
   }
 
 }
